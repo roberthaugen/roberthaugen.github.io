3b9ee0f4ba1fdb8320ab7e28abbcde2f3463507b
Robert Haugen <robert@haugen.me>
2019-09-28 19:04:33 +0200

    deleted da shizzle!

--- component---src-pages-index-js-f4839eb3d58e6cacd737.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{188:function(n,e,t){"use strict";t.r(e);var a=t(0),o=t.n(a);e.default=function(){return o.a.createElement("h1",null,"Hello Gatsby!")}}}]);
-//# sourceMappingURL=component---src-pages-index-js-f4839eb3d58e6cacd737.js.map
